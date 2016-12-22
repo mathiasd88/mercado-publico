1c52864fb4a29f7944e031713488fc83064509de
Mathías Donoso <mathiasd88@gmail.com>
2016-12-21 22:13:21 -0300

    isValid method and test..

--- src/MercadoPublico.php
+++ src/MercadoPublico.php
@@ -2,7 +2,24 @@
 
 namespace Mathiasd88\MercadoPublico;
 
+use GuzzleHttp\Client;
+
 class MercadoPublico
 {
+    protected $ticket;
 
+    public function __construct($ticket)
+    {
+        $this->ticket = $ticket;
+        $this->url = 'http://api.mercadopublico.cl/servicios/v1/publico';
+    }
+
+    public function isValid()
+    {
+        $json = file_get_contents($this->url . '/licitaciones.json?ticket=' . $this->ticket);
+        
+        $response = json_decode($json);
+        
+        return ($response->Mensaje != 'Ticket no válido.');
+    }
 }
